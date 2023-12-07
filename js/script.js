4a7bf1ba125288b385232ad3570bd8430506a57c
Luis Ruiz <luisruiz4314@gmail.com>
2023-12-07 04:00:09 -0600

    Avances

--- js/script.js
+++ js/script.js
@@ -1,5 +1,83 @@
 
+function cambiarDiv() {
+  // Obtén el contenedor principal
+  var container = document.querySelector('.container-middle');
 
+  // Crea un nuevo textarea con la clase 'for-dinamico'
+  var nuevoTextarea = document.createElement('textarea');
+  nuevoTextarea.className = 'for-dinamico';
+  nuevoTextarea.rows = 5; // Establece el número de filas según tus necesidades
+  nuevoTextarea.cols = 40; // Establece el número de columnas según tus necesidades
+  // nuevoTextarea.value = 'Nuevo contenido dinámico...';
+
+  // Agrega el nuevo textarea al contenedor
+  container.replaceChild(nuevoTextarea, document.querySelector('.for'));
+
+  document.querySelector('.container-terminal').style.display = 'block';
+
+  document.querySelector('.container-button').style.display = 'none';
+
+}
+
+function cargarGramatica() {
+  var input = document.createElement('input');
+  input.type = 'file';
+
+  input.addEventListener('change', function () {
+    var file = input.files[0];
+
+    if (file) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        var contenido = e.target.result;
+        // Muestra el contenido del archivo en el textarea
+        document.querySelector('.for-dinamico').value = contenido;
+
+      };
+
+      reader.readAsText(file);
+    }
+  });
+
+  // Abre el cuadro de diálogo para seleccionar un archivo
+  input.click();
+}
+
+function cargarEjemplo() {
+  var input = document.createElement('input');
+  input.type = 'file';
+
+  input.addEventListener('change', function () {
+    var file = input.files[0];
+
+    if (file) {
+      var reader = new FileReader();
+
+      reader.onload = function (e) {
+        var contenido = e.target.result;
+
+        var contenidoSinSaltos = eliminarSaltosDeLinea(contenido);
+
+        // Muestra el contenido del archivo en el textarea
+        document.querySelector('.for-dinamico').value = contenido;
+
+        analizarSintaxis(contenidoSinSaltos)
+
+      };
+
+      reader.readAsText(file);
+    }
+  });
+
+  // Abre el cuadro de diálogo para seleccionar un archivo
+  input.click();
+}
+
+function eliminarSaltosDeLinea(texto) {
+  // Utiliza una expresión regular para eliminar saltos de línea
+  return texto.replace(/(\r\n|\n|\r)/gm, "");
+}
 
 function recorrerCadena(cadena) {
   for (i = 0; i < cadena.length; i++) {
@@ -278,13 +356,203 @@ function tokenOperadorRelacional(pCadena, pIndex) {
   // }
 }
 
+function tokenOperadorLogico(pCadena, pIndex) {
+  let cadena = pCadena;
+  let index = pIndex;
+  let indexAux = index;
+  let tokenOpLog = ["&&", "||"];
+  let aux;
+
+  for (i = 0; i < tokenOpLog.length; i++) { // recorre el token para encontrar una letra
+    aux = 0;
+    for (j = 0; j < tokenOpLog[i].length; j++) { // recorre el token para 
+      if (tokenOpLog[i][j] === cadena[indexAux]) {
+        aux++;
+        indexAux++;
+      }
+    }
+    if (aux == 2) {
+      index = indexAux;
+      break;
+    } else {
+      index = -1;
+    }
+  }
+
+  return index;
+}
+
 function analizarSintaxis(param_cadena) {
-  const cadena = "for(i = 0; i == j){}}"
+  // const cadena = "for (i = 0 ; i==j || i == 0 ; ) { } }"
+  let cadena = param_cadena
   let index = 0;
   let indexAux = 0;
+  let msgError = "";
+  let msgOk = "Todo está OK";
+  let isError = false;
 
-  index = tokenEspacios(cadena, index)
+  index = tokenEspacios(cadena, index) // evalúa espacios
 
+  index = tokenFor(cadena, index);
+  if (index != -1) { // si for válido
+    index = tokenEspacios(cadena, index) // evalúa espacios
+
+    index = tokenParentesisApertura(cadena, index);
+    if (index != -1) { // si existe '('
+      index = tokenEspacios(cadena, index) // evalúa espacios
+
+      indexAux = tokenPuntoYComa(cadena, index);
+      if (indexAux != -1) { // si existe ';'
+        index = indexAux;
+      } else {
+        index = tokenIdentificador(cadena, index);
+        if (index != -1) { // si identificador válido
+          index = tokenEspacios(cadena, index) // evalúa espacios
+
+          indexAux = tokenSimboloIgual(cadena, index);
+          if (indexAux != -1) { // si existe '='
+            index = indexAux;
+            index = tokenEspacios(cadena, index) // evalúa espacios
+
+            indexAux = tokenValorIdentificador(cadena, index);
+            if (indexAux != -1) {
+              index = indexAux
+
+              index = tokenEspacios(cadena, index) // evalúa espacios
+
+              /** puede seguir más, no solo ';' */
+
+              indexAux = tokenPuntoYComa(cadena, index);
+              if (indexAux != -1) { // si existe punto y coma ';'
+                index = indexAux;
+              } else {
+                isError = true
+                msgError = "Error: punto y coma. \nSe esperaba ';'"
+              }
+
+            } else {
+              isError = true
+              msgError = 'Error, se esperaba un identificador o valor válido'
+            }
+
+          } else {
+            isError = true
+            msgError = "Error: '='. \nSe esperaba un signo de igualdad"
+          }
+
+        } else {
+          isError = true
+          msgError = "Error: identificador no válido. \nSe esperaba identificador válido"
+        }
+      }
+
+      // continuación después del exp_ini ;
+      if (!isError) {
+        index = tokenEspacios(cadena, index) // evalúa espacios
+
+        do {
+          isOpLog = false;
+          indexAux = tokenNegacion(cadena, index);
+          if (indexAux != -1) { // si encontró el !, entonces, evalúa valor o variable
+            index = indexAux;
+
+            index = tokenEspacios(cadena, index) // evalúa espacios
+
+            indexAux = tokenValorIdentificador(cadena, index);
+            if (indexAux != -1) { // si identificador válido
+              index = indexAux
+            } else {
+              isError = true
+              msgError = "Error: identificador no válido. \nSe esperaba identificador válido"
+            }
+
+          } else {
+            indexAux = tokenValorIdentificador(cadena, index);
+            if (indexAux != -1) { // si identificador válido
+              index = indexAux
+              index = tokenEspacios(cadena, index) // evalúa espacios
+
+              indexAux = tokenOperadorRelacional(cadena, index);
+              if (indexAux != -1) { // si existe operador relacional
+                index = indexAux;
+                index = tokenEspacios(cadena, index); // evalúa espacios
+
+                indexAux = tokenValorIdentificador(cadena, index);
+                if (indexAux != -1) {
+                  index = indexAux
+                  index = tokenEspacios(cadena, index) // evalúa espacios
+
+                  indexAux = tokenOperadorLogico(cadena, index);
+                  if (indexAux != -1) {
+                    index = indexAux
+                    isOpLog = true
+                    index = tokenEspacios(cadena, index) // evalúa espacios
+                  }
+
+                } else {
+                  isError = true
+                  msgError = 'Error, se esperaba un identificador o valor válido'
+                }
+              } else {
+                isError = true
+                msgError = "Error, se esperaba un operador relacional"
+              }
+            } else {
+              isError = true
+              msgError = 'Error, se esperaba un identificador o valor válido'
+            }
+          }
+
+        } while (isOpLog && !isError)
+
+        if (!isError) {
+          indexAux = tokenPuntoYComa(cadena, index);
+          if (indexAux != -1) { // si existe ';'
+            index = indexAux;
+
+            index = tokenEspacios(cadena, index); // evalúa espacios
+          } else {
+            isError = true
+            msgError = "Error: punto y coma. \nSe esperaba ';'"
+          }
+        }
+
+      }
+    } else {
+      msgError = "Error: paréntesis. \n Se esperaba paréntesis de apertura: '('"
+    }
+  } else {
+    msgError = "Error: Palabra reservada no válida. \nSe esperaba un 'for'"
+  }
+
+  var miDiv = document.querySelector(".body-terminal");
+
+  if (isError) {
+
+    // Agrega una nueva clase al div
+    miDiv.classList.add("isError");
+
+    console.log(msgError)
+    msgTerminal = msgError
+  } else {
+
+    // Agrega una nueva clase al div
+    miDiv.classList.add("isOk");
+
+    console.log(msgOk)
+    msgTerminal = msgOk
+  }
+
+  console.log(cadena)
+  console.log("'" + cadena[index] + "' [" + index + "]")
+
+  var miDiv = document.querySelector(".body-terminal");
+
+  // Agrega texto utilizando textContent
+  miDiv.textContent = msgTerminal;
+}
+
+function secuencia() {
   // 1. evalúa: for
   index = tokenFor(cadena, index);
   if (index == -1) {
@@ -357,6 +625,10 @@ function analizarSintaxis(param_cadena) {
   // 11. evalúa espacios en blanco
   index = tokenEspacios(cadena, index);
 
+
+
+
+
   // 12. evalúa negación
   indexAux = tokenNegacion(cadena, index);
   if (indexAux != -1) { // si encontró el !, entonces, evalúa valor o variable
@@ -394,16 +666,16 @@ function analizarSintaxis(param_cadena) {
       console.log("Error, se esperaba un operador relacional");
     }
 
-    // 11. evalúa espacios en blanco
-    index = tokenEspacios(cadena, index);
+    // // 11. evalúa espacios en blanco
+    // index = tokenEspacios(cadena, index);
 
-    // valor numerico o identi
-    indexAux = tokenValorIdentificador(cadena, index);
-    if (indexAux != -1) {
-      index = indexAux
-    } else {
-      console.log('Error, se esperaba un identificador o valor válido')
-    }
+    // // valor numerico o identi
+    // indexAux = tokenValorIdentificador(cadena, index);
+    // if (indexAux != -1) {
+    //   index = indexAux
+    // } else {
+    //   console.log('Error, se esperaba un identificador o valor válido')
+    // }
   }
 
 
@@ -427,9 +699,57 @@ function analizarSintaxis(param_cadena) {
 }
 
 function main() {
-  cadena = "for(i=0; i >= 10){}"
+  cadena = "for(i=0; i >= 10;){}"
   analizarSintaxis(cadena);
 }
 
-main();
+// main();
 
+// function ejemplosFor() {
+//   for(i = 0; i < 15; i++) {
+//     a = b;
+//   }
+
+//   for(i = 0; i < 15; i++) {
+//     a = miVariable + i;
+//   }
+
+//   for(indiceAux = indice; indiceAux >= 500; indiceAux--) {
+//     a = indiceAux;
+//     b = a * (2 * indiceAux);
+//   }
+
+//   for(i = 0; i == 10 || i >= a; i++) {
+//     a = b + i;
+//   }
+
+//   for(i = 0; i < 15 && a != i; i++) {
+//     a = mivar * i;
+//   }
+
+//   for(j = 0; j = 7; j++) {
+//     a = i;
+//   }
+
+//   for (i = j; i * 7; i--) {
+//     a = 5 * param;
+//   }
+
+//   for(10 = i; i != 1; i++) {
+//     a = 1;
+//     b = 2;
+//   }
+
+//   forr (i=0; i==100; i++) {
+//     a = b*i;
+//   }
+
+//   for (indice = 150; indice > cadena; indice--) {
+//     a = indice * 2;
+//     b = a - 5 + (3 * indice);
+//     mivariable = 300;
+//     c += 5;
+//     x++;
+//     z = a + b + c
+//   }
+// }
